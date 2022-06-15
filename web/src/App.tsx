baabc36c40ede401b9003aecdd4df7e0d6f0a072
alex <1353716+alexstojda@users.noreply.github.com>
2022-06-14 20:56:07 -0400

    chore: install and setup tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 function App() {
   return (
-    <h2>Hello World</h2>
+    <div className="min-h-screen w-screen p-5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <Card>
+        <h1 className="text-4xl font-black">
+          Alex Stojda
+        </h1>
+      </Card>
+    </div>
   );
+}
+
+function Card({children}: { children: React.ReactNode }) {
+  return (
+    <div className="container mx-auto bg-white/30 p-5 rounded-lg drop-shadow-md">
+      {children}
+    </div>
+  )
 }
 
 export default App;
